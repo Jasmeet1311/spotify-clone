437d2f28bf4920c109faee73ee25ed80f6af26c2
Jasmeet Kaur <jasmeet13112001@gmail.com>
2024-04-16 20:06:21 +0530

    Songs added in library section and seekbar added

--- script.js
+++ script.js
@@ -10,17 +10,31 @@ async function getSongs() {
   for (let i = 0; i < a.length; i++) {
     const element = a[i];
     if (element.href.endsWith(".mp3")) {
-      song.push(element.href);
+      song.push(element.href.split("/songs/")[1]);
     }
   }
   return song;
 }
 async function main() {
   let songs = await getSongs();
-  console.log(songs);
+  // console.log(songs);
+  let songUL = document.getElementsByTagName("ol")[0];
+  // console.log(songUL);
+  for (const song of songs) {
+    songUL.innerHTML = songUL.innerHTML + `
+    <li class="flex">
+                    <img class="invert p-1" src="https://cdn.hugeicons.com/icons/music-note-03-stroke-rounded.svg" alt="music-note-03" width="24" height="24" />
+                    <div class="info">
+                      <div class="song-name" style="padding: 10px">${song.replaceAll("%20"," ")}</div>
+                    </div>
+                    <img class="invert" src="https://cdn.hugeicons.com/icons/play-circle-solid-rounded.svg" alt="play-circle" width="24" height="24" />
+                  </li>`;
+  }
+  
+  // console.log(songUL);
   var audio = new Audio(songs[0]);
-  audio.play();
-  audio.pause();
+  // audio.play();
+  // audio.pause();
   audio.addEventListener("loadeddata", () => {
     let duration = audio.duration;
     console.log(duration/60);
